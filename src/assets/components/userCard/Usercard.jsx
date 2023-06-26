7736d45f6e73176bc4920bb714d41de29563d32b
Michelle Vegas <michellevegas_16@hotmail.com>
2023-06-26 19:04:39 -0300

    like button

--- src/assets/components/userCard/Usercard.jsx
+++ src/assets/components/userCard/Usercard.jsx
@@ -1,12 +1,23 @@
 import "./styles/userCardStyle.css"
+import { useState } from "react";
 import PropTypes from 'prop-types';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 function UserCard({ user }) {
+  const [likes, setLikes] = useState(0);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
   return (
     <div className="user-card">
       <h3 className="user-name">{user.nickname}</h3>
       <p>Idade: {user.idade}</p>
       <p>Email: {user.email}</p>
+      <div className="like-container">
+        <FontAwesomeIcon className="like-button" icon={faHeart} onClick={handleLike}/>
+        <span className="like-count">{likes}</span>
+      </div>
     </div>
   );
 }
